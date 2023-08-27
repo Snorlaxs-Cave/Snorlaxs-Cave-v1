c07fb2cda8aed914dcdf213e334e58b45f540749
Derpman <immapotato8110@gmail.com>
2023-08-27 16:12:26 -0400

    close current window when tab cloaked

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -177,8 +177,9 @@ function cloak() {
     let url = window.location.href;
     var w = window.open("about:blank", "_blank");
     w.document.write('<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="' + url + '"></iframe>');
+    window.close('','_parent','');
   }
 
-  function ourDiscord () {
+function ourDiscord () {
     window.location.href = 'https://discord.gg/Cqaa8x82Ch';
-    }
+}
