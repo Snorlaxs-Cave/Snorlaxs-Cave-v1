3462c2b6a320eb124dc84910c3c0c4910dc58f7c
snorlax <143361136+snorlaxscave@users.noreply.github.com>
2023-08-28 04:35:23 +0000

    moto-x3m js

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -249,6 +249,16 @@ function sfnf () {
 
 }
 
+function moto () {
+
+    window.navigator.serviceWorker.register('/sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://moto-x3m.net/');
+    });
+
+}
+
 function emulatorJS () {
    location.href = '/emulatorjs';
 }
