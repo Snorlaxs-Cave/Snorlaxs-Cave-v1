eb40473ed599312b92afa31abe2ad27b5c7d648b
snorlax <143361136+snorlaxscave@users.noreply.github.com>
2023-08-28 03:54:25 +0000

    snorlax game

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -229,6 +229,16 @@ function onevonelol () {
 
 }
 
+function sfnf () {
+
+    window.navigator.serviceWorker.register('/sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://kbhgames.com/game/friday-night-funkin-vs-snorlax');
+    });
+
+}
+
 function emulatorJS () {
    location.href = '/emulatorjs';
 }
