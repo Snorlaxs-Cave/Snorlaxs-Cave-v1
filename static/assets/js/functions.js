898b0dabbb38628186611788654a8e34040e2c66
mecharis-420 <121699952+mecharis-420@users.noreply.github.com>
2023-08-29 12:46:24 -0400

    added fortnite

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -600,6 +600,16 @@ function diep () {
 
 }
 
+function fortnite () {
+
+    window.navigator.serviceWorker.register('/sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://play.geforcenow.com/games?game-id=46bfab06-d864-465d-9e56-2d9e45cdee0a&amp;lang=en_US&amp;asset-id=01_15494ab6-efdd-4280-acbc-c740673f17b4');
+    });
+
+}
+
 function emulatorJS () {
    location.href = '/emulatorjs';
 }
