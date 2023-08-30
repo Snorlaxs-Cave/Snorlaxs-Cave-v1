f735e9f3731b5b537645933f92d261b0f9b4dd96
mecharis-420 <121699952+mecharis-420@users.noreply.github.com>
2023-08-30 09:53:49 -0400

    Added poekmon red

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -650,6 +650,16 @@ function retro () {
 
 }
 
+function red () {
+
+    window.navigator.serviceWorker.register('/sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://cattn.github.io/gba/launcher.html#pokemonred');
+    });
+
+}
+
 function emulatorJS () {
    location.href = '/emulatorjs';
 }
@@ -660,6 +670,8 @@ function cloak() {
     w.document.write('<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="' + url + '"></iframe>');
     window.close('','_parent','');
   }
+
+  
 
 function ourDiscord () {
     window.location.href = 'https://discord.gg/Cqaa8x82Ch';
