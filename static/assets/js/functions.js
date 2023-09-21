926ecff39bb9cb00e5c5e98ad6c940f8dc805f04
mecharis-420 <121699952+mecharis-420@users.noreply.github.com>
2023-09-21 10:12:50 -0400

    blah

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -851,7 +851,7 @@ function krunker () {
     window.navigator.serviceWorker.register('/sw.js', {
         scope: __uv$config.prefix
     }).then(() => {
-        location.href=__uv$config.prefix + __uv$config.encodeUrlZ('https://krunker.gettingoverschool.repl.co/');
+        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://krunker.gettingoverschool.repl.co/');
     });
 
 }  
