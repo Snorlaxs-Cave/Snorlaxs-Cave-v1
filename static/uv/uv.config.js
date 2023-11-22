505f4928cc3f834c1ece9915413da99b05468f55
snorlax <143361136+snorlaxscave@users.noreply.github.com>
2023-11-22 09:53:37 +0000

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -37,7 +37,7 @@ try {
                     });
 
                     backBtn.addEventListener('mousedown', function (e) {
-                        dragElement(backBtnDiv);
+                        toggleButton(backBtnDiv);
                     });
 
                     console.log("Event listeners added");
@@ -58,39 +58,15 @@ try {
         }
     });
 
-    function dragElement(elmnt) {
-        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        if (document.getElementById(elmnt.id + "header")) {
-            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-        } else {
-            elmnt.onmousedown = dragMouseDown;
-        }
+    const button = document.getElementById('toggleButton');
+    button.style.display = localStorage.getItem('buttonVisible') === 'false' ? 'none' : 'block';
 
-        function dragMouseDown(e) {
-            e = e || window.event;
-            e.preventDefault();
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            document.onmouseup = closeDragElement;
-            document.onmousemove = elementDrag;
-        }
-
-        function elementDrag(e) {
-            e = e || window.event;
-            e.preventDefault();
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        }
-
-        function closeDragElement() {
-            document.onmouseup = null;
-            document.onmousemove = null;
-        }
-    }
-} catch (e) {
-    console.error(e);
-}
+    document.addEventListener('keydown', function(event) {
+      if (event.key === '1') {
+        button.style.display = button.style.display === 'none' ? 'block' : 'none';
+        localStorage.setItem('buttonVisible', button.style.display !== 'none');
+      }
+    });
+    window.onload = function() {
+      alert("TO HIDE THE GO BACK BUTTON PRESS 1, TO BRING IT BACK PRESS 1 AGAIN");
+    };
